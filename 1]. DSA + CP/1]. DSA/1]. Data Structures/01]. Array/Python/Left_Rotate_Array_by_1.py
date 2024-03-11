b7b4dd5ddc1431f076b8a1aa8136f1a2a1b16d57
siddmi <mishra07.siddharth@gmail.com>
2024-03-11 18:23:07 +0530

    Adding updated approaches for these two methods in array

--- 1]. DSA + CP/1]. DSA/1]. Data Structures/01]. Array/Python/Left_Rotate_Array_by_1.py
+++ 1]. DSA + CP/1]. DSA/1]. Data Structures/01]. Array/Python/Left_Rotate_Array_by_1.py
@@ -1,21 +1,34 @@
 """
-Function to rotate an the first element
+Function to rotate the array
 """
 
 
-def left_rotate(arr, num):
-    tmp = arr[0]
+# def left_rotate(arr, num):
+#     tmp = arr[0]
 
-    for index in range(1, num):
-        arr[index - 1] = arr[index]
+#     for index in range(1, num):
+#         arr[index - 1] = arr[index]
 
-    arr[num - 1] = tmp
+#     arr[num - 1] = tmp
 
+# arr = [1, 2, 3, 4, 5]
+# num = 4
+
+# print("Before Left Rotation:", arr)
+
+# left_rotate(arr, num)
+# print("After '1' Left Rotation:", arr)
+
+
+def left_rotate_by_1(arr, index):
+    tmp = arr.pop(0)
+    arr = arr[:index - 1] + [tmp] + arr[index - 1:]
+    return arr
 
 arr = [1, 2, 3, 4, 5]
-num = 2
+num = 4
 
 print("Before Left Rotation:", arr)
 
-left_rotate(arr, num)
-print("After '1' Left Rotation:", arr)
+new_arr = left_rotate_by_1(arr, num)
+print("After '1' Left Rotation:", new_arr)
--- 1]. DSA + CP/1]. DSA/1]. Data Structures/01]. Array/Python/Reverse_Array.py
+++ 1]. DSA + CP/1]. DSA/1]. Data Structures/01]. Array/Python/Reverse_Array.py
@@ -4,18 +4,25 @@ Function to Reverse an Array
 Takes in an Array and Returns an New Array
 """
 
+# Classical approach - Using for loop
+# def reverse(arr):
+#     new_arr = []
 
-def reverse(arr):
-    new_arr = []
+#     for index in range(len(arr)):
+#         new_arr.insert(0, arr[index])
 
-    for index in range(len(arr)):
-        new_arr.insert(0, arr[index])
+#     return new_arr
 
-    return new_arr
+# arr = [1, 2, 3, 4, 5]
+# new_arr = reverse(arr)
+
+# Faster approach - Using list slicing
+def reverse2(arr):
+    return arr[::-1]
 
 
 arr = [1, 2, 3, 4, 5]
-new_arr = reverse(arr)
+new_arr = reverse2(arr)
 
 print("Before Reverse:", arr)
 print("After Reverse:", new_arr)
