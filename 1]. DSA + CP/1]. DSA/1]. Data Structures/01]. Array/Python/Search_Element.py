f6fe907ea8d8bb14d321db55b45748af699d15e6
siddmi <mishra07.siddharth@gmail.com>
2024-03-11 09:15:28 +0530

    Updating names for DSA - array - python, also updating search element for array

---
+++ 1]. DSA + CP/1]. DSA/1]. Data Structures/01]. Array/Python/Search_Element.py
@@ -0,0 +1,25 @@
+# function to perform linear search for element in array
+def search(arr, element):
+    # Classical approach - Using for loop to check index of element
+    # for index in range(0, len(arr)):
+    #     if arr[index] == element:
+    #         return index
+        
+    # # if no such number exist in the array return -1
+    # return -1
+
+    # Efficient approach - Using list comprehension
+    result = next((i for i, x in enumerate(arr) if x == element), -1)
+    # Return the result of linear search
+    return result
+
+
+arr = [20, 5, 7, 25]
+element = 1
+
+print("Array Searched:", arr)
+print("Searching for:", element)
+if search(arr, element) == -1:
+    print("Element not found")
+else:
+    print("Searched index =", search(arr, element))
